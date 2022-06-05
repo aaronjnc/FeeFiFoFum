a4481eab189aeb11c78e74cb41231a57483bcbae
Aaron <aaronkingnc@att.net>
2022-06-05 01:28:54 -0400

    clear project

--- Source/FeeFiFoFum/FeeFiFoFum.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FeeFiFoFum.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FeeFiFoFum, "FeeFiFoFum" );
- 
